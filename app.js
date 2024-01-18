33f23c9d17d9111966179dc76ddf2d97d3548d81
Cadú <139708523+mrcadu7@users.noreply.github.com>
2024-01-18 18:24:02 -0300

    feat: adição das categorias e suas listagens

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 // Módulos
     const express = require('express');
-    const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+    const handlebars = require('express-handlebars').create({
+        defaultLayout: 'main', runtimeOptions: {
+            allowProtoPropertiesByDefault: true,
+            allowProtoMethodsByDefault: true
+        } 
+    });
     const bodyParser = require('body-parser');
     const app = express();
     const admin = require('./routes/admin');
     const path = require('path');
-    // const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
+    const session = require('express-session');
+    const flash = require('connect-flash');
+    
 
 
 // Configurações
+    // Session
+        app.use(session({
+            secret: 'nodeblog',
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash());
+
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg');
+            res.locals.error_msg = req.flash('error_msg');
+            next();
+        })
 
     // Body Parser
         app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,11 +41,21 @@
         app.set('view engine', 'handlebars');
 
     // Mongoose
-        // mongoose.connect('mongodb://localhost/blog');
+        mongoose.Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/nodeblog').then(() => {
+            console.log('Conectado ao MongoDB');
+        }).catch((err) => {
+            console.log('Erro ao conectar ao MongoDB' + err);
+        });
+        
     
     // Public
         app.use(express.static(path.join(__dirname, 'public')))
 
+        app.use((req, res, next) => {
+            console.log('ola sou o middleware...');
+            next();
+        })
 
 // Rotas
     app.get('/', (req, res) => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
+
+
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -10,12 +15,52 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render('admin/categorias')
+    Categoria.find().sort({date: 'desc'}).then((categorias) => {
+        res.render('admin/categorias', {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao listar as categorias')
+        res.redirect('/admin')
+    })
 })
 
 router.get('/categorias/add', (req, res) => {
     res.render('admin/addcategorias')
 })
+
+router.post('/categorias/nova', (req, res) => {
+
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: 'Nome inválido'})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: 'Slug inválido'})
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: 'Nome muito pequeno'})
+    }
+
+    if(erros.length > 0){
+        res.render('admin/addcategorias', {erros: erros})
+    }else{
+        const novaCat = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCat).save().then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso')
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash('error_msg', 'Erro ao salvar categoria, tente novamente')
+            res.redirect('/admin')
+        })
+    }
+})
+
 
 router.get('/teste', (req, res) => {
     res.send('Pagina de teste')
